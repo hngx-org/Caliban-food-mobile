78d51cd21dc427f1f6a3411f9e655a9f468efdb6
“Esther <essyc14@gmail.com>
2023-09-21 22:26:27 +0300

    Skeleton

---
+++ app/src/main/java/com/essycynthia/calibanfoodmobile/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.essycynthia.calibanfoodmobile
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.essycynthia.calibanfoodmobile.ui.theme.CalibanFoodMobileTheme
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            CalibanFoodMobileTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting()
+                }
+            }
+        }
+    }
+}
+@Composable
+fun Greeting(){
+    Text(text = "Yay it's working!")
+}
+
