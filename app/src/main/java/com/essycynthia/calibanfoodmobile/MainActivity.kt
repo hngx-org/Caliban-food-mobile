82d39ecb83c985ba335cce6c05fab48b6b92801c
Devraj Singh <105633824+2202devraj@users.noreply.github.com>
2023-09-22 20:16:58 +0530

    Update MainActivity.kt

--- app/src/main/java/com/essycynthia/calibanfoodmobile/MainActivity.kt
+++ app/src/main/java/com/essycynthia/calibanfoodmobile/MainActivity.kt
@@ -59,6 +59,8 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        // for splash screen
+        installSplashScreen()
 
         setContent {
             CalibanFoodMobileTheme {
