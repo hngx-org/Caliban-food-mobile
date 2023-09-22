a3efb27cc84d4488014cc8adb667ee5a8190f4c8
Nickyshe <judypearls@200>
2023-09-22 14:34:05 +0100

    made changes to gradle version and added another screen

---
+++ app/src/main/java/com/essycynthia/calibanfoodmobile/organizationfreelunch/CardData.kt
@@ -0,0 +1,7 @@
+package com.essycynthia.calibanfoodmobile.organizationfreelunch
+data class CardData
+(val userImageId: Int,
+                    val user: String,
+                    val lunchReward: Int,
+                    val time: Int
+)
