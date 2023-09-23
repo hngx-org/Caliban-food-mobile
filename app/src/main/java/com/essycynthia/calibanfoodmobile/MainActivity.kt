43740eed9bdcf95852dbe03dcac69542dffc6a6c
“Esther <essyc14@gmail.com>
2023-09-23 19:50:38 +0300

    corrected master

--- app/src/main/java/com/essycynthia/calibanfoodmobile/MainActivity.kt
+++ app/src/main/java/com/essycynthia/calibanfoodmobile/MainActivity.kt
@@ -32,6 +32,19 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.navigation.NavController
+import com.essycynthia.calibanfoodmobile.dummyData.RewardHistory
+import com.essycynthia.calibanfoodmobile.ui.BottomScreens
+import com.essycynthia.calibanfoodmobile.ui.Navigation
 
 import com.essycynthia.calibanfoodmobile.ui.theme.CalibanFoodMobileTheme
 import dagger.hilt.android.AndroidEntryPoint
