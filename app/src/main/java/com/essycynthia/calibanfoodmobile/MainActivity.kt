951d0720e9cb98762a68635472b91a306e496699
Nickyshe <judypearls@200>
2023-09-24 02:00:00 +0100

    HomepageUI set

--- app/src/main/java/com/essycynthia/calibanfoodmobile/MainActivity.kt
+++ app/src/main/java/com/essycynthia/calibanfoodmobile/MainActivity.kt
@@ -13,6 +13,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -94,25 +95,27 @@ fun FreeLunchDashBoard(
             containerColor = Color(0xFFFF9405)
         ),
         shape = RoundedCornerShape(size = (5.dp)),
-
-
         modifier = modifier
             //.clip(shape = ShapeDefaults.Small)
             .fillMaxWidth()
-            .height(200.dp)
-            .padding(start = 16.dp, end = 16.dp)
+            .height(170.dp)
+            .padding(start = 16.dp, end = 16.dp, )
 
     ) {
         Column(
+
             verticalArrangement = Arrangement.SpaceEvenly,
-            modifier = modifier.padding(16.dp)
+            horizontalAlignment = Alignment.CenterHorizontally
+           // modifier = modifier.padding(16.dp)
         ) {
             Row(
+
                 horizontalArrangement = Arrangement.spacedBy(200.dp)
             ) {
 
                 Text(
                     text = "Free Lunch",
+                    modifier = Modifier.padding(top = 14.dp, start = 8.dp),
 
                     // H3 (Semi Bold)
                     style = TextStyle(
@@ -123,36 +126,38 @@ fun FreeLunchDashBoard(
                         letterSpacing = 0.02.sp,
                     )
                 )
-                /*
-                                Text(
-                                    text = "Free Lunch",
-                                    style = MaterialTheme.typography.headlineSmall,
-                                    color = MaterialTheme.colorScheme.onPrimary
-                                )*/
+
 
                 Image(
                     painter = painterResource(id = R.drawable.avatar),
                     contentDescription = "User Image",
                     modifier
+                        .padding(top = 14.dp, end = 8.dp)
                         .height(36.dp)
                         .width(36.dp)
                         .clip(shape = ShapeDefaults.Small)
                         .size(36.dp),
+
                     alignment = Alignment.TopEnd
 
                 )
             }
-            Spacer(modifier = Modifier.height(12.dp))
+          //  Spacer(modifier = Modifier.height(12.dp))
 
             Text(
-                text = "$freeLunchAmount",
-                style = MaterialTheme.typography.headlineSmall,
-                color = MaterialTheme.colorScheme.onPrimary,
-                textAlign = TextAlign.Center,
-                modifier = Modifier.fillMaxWidth() // Add this modifier
+                text = "10",
 
+                // H1 (Bold)
+                style = TextStyle(
+                    fontSize = 28.sp,
+                    fontFamily = FontFamily(Font(R.font.poppins)),
+                    fontWeight = FontWeight(700),
+                    color = Color(0xFFFFFFFF),
+                    letterSpacing = 0.07.sp,
+                    textAlign = TextAlign.Center
+                )
             )
-            Spacer(modifier = Modifier.height(12.dp))
+
 
             Button(
                 onClick = { navController.navigate(BottomScreens.WithdrawalScreen.route)},
@@ -161,13 +166,23 @@ fun FreeLunchDashBoard(
                     containerColor = MaterialTheme.colorScheme.onPrimary
                 ),
                 shape = ShapeDefaults.Small,
-                modifier = modifier.fillMaxWidth()
+                modifier = modifier
+
+                    .width(300.dp)
             ) {
                 Text(
                     text = "Withdraw Lunch",
-                    style = MaterialTheme.typography.bodyMedium,
-                    color = MaterialTheme.colorScheme.primary
+
+                    // Button
+                    style = TextStyle(
+                        fontSize = 12.sp,
+                        fontFamily = FontFamily(Font(R.font.roboto)),
+                        fontWeight = FontWeight(500),
+                        color = Color(0xFFFF9405),
+                        letterSpacing = 0.15.sp,
+                    )
                 )
+
             }
         }
     }
@@ -175,30 +190,49 @@ fun FreeLunchDashBoard(
 
 @Composable
 fun SendLunchReward(modifier: Modifier = Modifier) {
-    Surface(
-        color = MaterialTheme.colorScheme.onPrimary,
-        modifier = modifier
-            .clip(shape = RoundedCornerShape(10.dp))
-            .border(
-                width = 2.dp, // Adjust the border width as needed
-                color = Color.Black, // Adjust the border color as needed
-                shape = RoundedCornerShape(10.dp)
-            )
-            .fillMaxWidth()
-            .height(160.dp)
+
+    Card(
+        modifier = Modifier
             .padding(start = 16.dp, end = 16.dp, top = 20.dp)
-    ) {
+            .fillMaxWidth()
+            .width(343.dp)
+            .height(140.dp),
+        colors = CardDefaults.cardColors(
+            containerColor = Color(0xFFFFFFFF)
+        ),
+        shape = RoundedCornerShape(size = 10.dp)
+
+
+    )
+  {
         Row(
 
             horizontalArrangement = Arrangement.spacedBy(38.dp)
 
 
         ) {
-            Column {
+            Column(
+                verticalArrangement = Arrangement.spacedBy(0.dp, Alignment.CenterVertically),
+                horizontalAlignment = Alignment.CenterHorizontally,
+            ) {
                 Text(
                     text = "Recognize your colleague for their \n " +
                             "outstanding work by sending them \n" +
-                            "Free Lunch rewards. \uD83D\uDC4F \uD83C\uDF1F"
+                            "Free Lunch rewards. \uD83D\uDC4F \uD83C\uDF1F",
+                    modifier =Modifier
+                        .width(198.dp)
+                        .height(54.dp),
+
+// Small Body (Regular)
+                    style = TextStyle(
+                        fontSize = 12.sp,
+                        lineHeight = 18.sp,
+                        fontFamily = FontFamily(Font(R.font.roboto)),
+                        fontWeight = FontWeight(400),
+                        color = Color(0xFF333333),
+                        letterSpacing = 0.03.sp,
+                    )
+
                 )
 
                 Button(
@@ -208,11 +242,24 @@ fun SendLunchReward(modifier: Modifier = Modifier) {
                     colors = ButtonDefaults.buttonColors(Color(0xFFFF9405)),
                     shape = ShapeDefaults.Small
                 ) {
+
                     Text(
+                        text = "Send Lunch Reward",
+
+                        // Button
+                        style = TextStyle(
+                            fontSize = 12.sp,
+                            fontFamily = FontFamily(Font(R.font.roboto)),
+                            fontWeight = FontWeight(500),
+                            color = Color(0xFFFFFFFF),
+                            letterSpacing = 0.15.sp,
+                        )
+                    )
+                  /*  Text(
                         text = "Send Lunch Reward",
                         style = MaterialTheme.typography.bodyMedium,
                         color = MaterialTheme.colorScheme.onPrimary
-                    )
+                    )*/
                 }
             }
 
@@ -220,8 +267,14 @@ fun SendLunchReward(modifier: Modifier = Modifier) {
                 painter = painterResource(id = R.drawable.interracial),
                 contentDescription = "An Interracial couple",
                 Modifier
+                    .fillMaxWidth()
+                    .fillMaxHeight()
+                   // .width(154.00008.dp)
+                   //.height(154.81921.dp)
+                  //  .padding(top = 2.45745.dp, end = 5.73405.dp, bottom = 3.27659.dp)
+              /*  Modifier
                     .height(250.dp)
-                    .width(30.dp)
+                    .width(30.dp)*/
             )
         }
 
@@ -234,11 +287,52 @@ fun RewardHistoryList(
     rewardHistories: List<RewardHistory>,
     modifier: Modifier = Modifier
 ) {
-    Row() {
-        Text(text = "Reward History",modifier = Modifier.padding(horizontal = 12.dp))
-        Spacer(Modifier.width(200.dp))
-        Text(text = "See All", color = Color(0xFFFF9405), fontSize = 10.sp)
+    //make lazy list
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()//check
+            .padding(top = 20.dp, bottom = 15.dp),
+        horizontalArrangement = Arrangement.spacedBy(160.dp, Alignment.Start),
+        verticalAlignment = Alignment.Top,
+    ) {
+
+        Text(
+            text = "Reward history",
+            modifier = Modifier
+                .padding(start = 15.dp)
+                .width(128.dp),
+
+
+
+            // Body Text (Medium)
+            style = TextStyle(
+                fontSize = 14.sp,
+                fontFamily = FontFamily(Font(R.font.robotomedium)),
+                fontWeight = FontWeight(500),
+                color = Color(0xFF75818F),
+                letterSpacing = 0.07.sp,
+            )
+        )
+        // Child views.
+
+        Text(
+            text = "See all",
+            modifier = Modifier
+                .padding()
+                .width(39.dp)
+                .height(15.dp),
+
+            // Small Body (Medium)
+            style = TextStyle(
+                fontSize = 13.sp,
+                fontFamily = FontFamily(Font(R.font.robotomedium)),
+                fontWeight = FontWeight(500),
+                color = Color(0xFFFF9405),
+                letterSpacing = 0.03.sp,
+            )
+        )
     }
+
 
     LazyColumn(
         modifier = modifier,
@@ -258,7 +352,17 @@ fun RewardHistoryList(
     fun RewardHistoryElement(
         rewardHistory: RewardHistory
     ) {
-        Column {
+        Card (
+            modifier = Modifier
+                .padding(top = 40.dp)
+                .width(344.dp)
+                .height(69.dp),
+            colors = CardDefaults.cardColors(
+                containerColor = Color(0xFFFFFFFF)
+            ),
+
+            shape = RoundedCornerShape(size = 5.dp)
+        ){
             Row(
                 verticalAlignment = Alignment.CenterVertically
             ) {
@@ -268,15 +372,74 @@ fun RewardHistoryList(
                     modifier = Modifier.size(30.dp)
                 )
                 Spacer(modifier = Modifier.width(8.dp))
-                Text(text = "Essy sent you a free lunch")
-                Spacer(modifier = Modifier.weight(1f))
-                Spacer(modifier = Modifier.width(8.dp))
+                Text(
+                    text = "Essy sent you a free Lunch",
+                    modifier = Modifier
+                        .padding(end = 35.dp)
+                        .width(144.dp)
+                        .height(18.dp),
+                    style = TextStyle(
+                        fontSize = 12.sp,
+                        lineHeight = 18.sp,
+                        fontFamily = FontFamily(Font(R.font.robotomedium)),
+                        fontWeight = FontWeight(400),
+                        color = Color(0xFF333333),
+                        letterSpacing = 0.03.sp,
+                    )
+                )
+               // Text(text = "Essy sent you a free lunch")
+                Text(
+                    text = "3",
+                    Modifier
+                        // .padding(end =5.dp)
+                        .width(27.dp)
+                        .height(24.dp),
+
+
+                    // H3 (Semi Bold)
+                    style = TextStyle(
+                        fontSize = 16.sp,
+                        fontFamily = FontFamily(Font(R.font.poppinsemibold)),
+                        fontWeight = FontWeight(600),
+                        color = Color(0xFFFF9405),
+                        letterSpacing = 0.02.sp,
+                    )
+                )
+
                 Image(
                     painter = painterResource(id = R.drawable.pot_of_food),
                     contentDescription = "Pot of Food",
-                    modifier = Modifier.size(30.dp)
+                    modifier = Modifier.size(20.dp)
                 )
             }
+
+            Column(
+                modifier = Modifier
+                    .width(314.dp)
+                    .height(47.dp),
+                verticalArrangement = Arrangement.spacedBy(2.dp, Alignment.Bottom),
+                horizontalAlignment = Alignment.End,
+            ) {
+
+                Text(
+                    text = "3hrs",
+                    modifier  = Modifier
+                        .width(20.dp)
+                        .height(15.dp),
+
+                    // Caption
+                    style = TextStyle(
+                        fontSize = 10.sp,
+                        lineHeight = 15.sp,
+                        fontFamily = FontFamily(Font(R.font.robotoregular)),
+                        fontWeight = FontWeight(400),
+                        color = Color(0xFF75818F),
+                        letterSpacing = 0.04.sp,
+                    )
+                )
+                // Child views.
+            }
+
         }
     }
 
